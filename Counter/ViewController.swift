f679e3878eb78d2dfe31b9e54602fd553e631555
DmitriyShilyaev <goodrus2010@yandex.ru>
2025-09-07 17:49:20 +0500

    Усложненная версия задания

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,19 +11,51 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var counter: UILabel!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var changeLog: UITextView!
     
     var counterScore = 0
     
+    private func updateChangeLog(with text: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "[dd.MM.yyyy HH:mm:ss]:"
+        let dateString = formatter.string(from: Date())
+        if let existingText = changeLog.text {
+             changeLog.text = existingText + "\n" + dateString + text
+                } else {
+                    changeLog.text = dateString
+                }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        counter.text = "Значение счётчика: 0"
+        counter.text = "«Значение счётчика: 0»"
+        changeLog.text = "История изменений:"
+        changeLog.isEditable = false
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func resetScoreButton(_ sender: Any) {
+        counterScore = 0
+        counter.text = "«Значение счётчика: \(counterScore)»"
+        updateChangeLog(with: " значение сброшено")
+    }
+    
+    @IBAction func decreaseScoreButton(_ sender: Any) {
+        
+        if counterScore == 0 {
+            counterScore = 0
+            updateChangeLog(with: " попытка уменьшить значение счётчика ниже нуля")
+        } else {
+            counterScore -= 1
+            updateChangeLog(with: " -1")}
+        
+        counter.text = "«Значение счетчика: \(counterScore)»"
+    }
+    
     @IBAction func increaseScoreButton(_ sender: Any) {
         counterScore += 1
-        counter.text = "Значение счётчика: \(counterScore)"
+        counter.text = "«Значение счётчика: \(counterScore)»"
+        updateChangeLog(with: " +1")
     }
 }
 
