9b230e7d193a1c281a4efe40749558f1a1b2cced
DmitriyShilyaev <goodrus2010@yandex.ru>
2025-09-07 13:11:06 +0500

    Базовая версия задания

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//r
 //  ViewController.swift
 //  Counter
 //
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    var counterScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counter.text = "Значение счётчика: 0"
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func increaseScoreButton(_ sender: Any) {
+        counterScore += 1
+        counter.text = "Значение счётчика: \(counterScore)"
+    }
 }
+
 
